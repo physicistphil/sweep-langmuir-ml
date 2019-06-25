11d9afdeaa8447c5ead74c2431c15388a2e41492
Phil Travis <physicistphil@gmail.com>
2019-06-25 14:08:21 -0700

    Added comet.ml integration

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # sweep-langmuir-ml
 Analyzing Langmuir probe sweeps using machine learning. 
+
+[Comet.ml page](https://www.comet.ml/physicistphil/sweep-langmuir-ml).
--- autoencoder/ae-001.py
+++ autoencoder/ae-001.py
@@ -2,3 +2,14 @@ import numpy as np
 # from functools import partial
 import tensorflow as tf
 # from datetime import datetime
+from comet_ml import experiment
+
+experiment = Experiment(project_name="sweep-langmuir-ml", workspace="physicistphil")
+
+n_inputs = 500
+
+with tf.name_scope("data"):
+    tf.placeholder(tf.float32, [None, n_inputs], name="X")
+
+# with tf.name_scope("network"):
+    # tf.initializers.
