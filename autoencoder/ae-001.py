1b0e443186a9fc7d8b6ddda665fb630e1881a31d
Phil Travis <physicistphil@gmail.com>
2019-07-02 11:09:36 -0700

    Removed comet.ml optimizer usage. Improved layer size configuration

--- autoencoder/ae-001.py
+++ autoencoder/ae-001.py
@@ -1,5 +1,4 @@
 from comet_ml import Experiment
-from comet_ml import Optimizer
 
 import tensorflow as tf
 import numpy as np
@@ -31,20 +30,25 @@ def build_graph(hyperparams):
     batch_norm = partial(tf.layers.batch_normalization, training=training,
                          momentum=hyperparams['momentum'])
 
+    size_l1 = 200
+    size_l2 = 100
+    size_l3 = 50
+    size_h = 20
+
     with tf.name_scope("nn"):
-        enc1 = dense_layer(X, 200, name="enc1")
+        enc1 = dense_layer(X, size_l1, name="enc1")  # no activation specified = linear
         enc_b1 = tf.nn.elu(batch_norm(enc1))
-        enc2 = dense_layer(enc_b1, 100, name="enc2")
+        enc2 = dense_layer(enc_b1, size_l2, name="enc2")
         enc_b2 = tf.nn.elu(batch_norm(enc2))
-        enc3 = dense_layer(enc_b2, 50, name="enc3")
+        enc3 = dense_layer(enc_b2, size_l3, name="enc3")
         enc_b3 = tf.nn.elu(batch_norm(enc3))
-        h_base = dense_layer(enc_b3, 20, name="h_base")
+        h_base = dense_layer(enc_b3, size_h, name="h_base")
         h_base_b = tf.nn.elu(batch_norm(h_base))
-        dec1 = dense_layer(h_base_b, 50, name="dec1")  # TODO: maybe implement weight tying
+        dec1 = dense_layer(h_base_b, size_l3, name="dec1")  # TODO: maybe implement weight tying
         dec_b1 = tf.nn.elu(batch_norm(dec1))
-        dec2 = dense_layer(dec_b1, 100, name="dec2")
+        dec2 = dense_layer(dec_b1, size_l2, name="dec2")
         dec_b2 = tf.nn.elu(batch_norm(dec2))
-        dec3 = dense_layer(dec_b2, 200, name="dec3")
+        dec3 = dense_layer(dec_b2, size_l1, name="dec3")
         dec_b3 = tf.nn.elu(batch_norm(dec3))
         output_layer = dense_layer(dec_b3, 500, name="output_layer")
         output_b = tf.nn.elu(batch_norm(output_layer))
@@ -134,17 +138,6 @@ def train(experiment, hyperparams):
 
 if __name__ == '__main__':
     experiment = Experiment(project_name="sweep-langmuir-ml", workspace="physicistphil")
-
-    opt_config = {"algorithm": "bayes",
-                  "parameters": {"scale": {"type": "float",
-                                           "scalingType": "loguniform",
-                                           "min": 0.0
-                                            },
-                                 "learning_rate": {},
-                                 "momentum": {}},
-                  "spec": {"metric": "loss",
-                           "objective": "minimize"}}
-
     hyperparams = {'n_inputs': 500,
                    'scale': 0.0,  # no regularization
                    'learning_rate': 1e-3,
@@ -152,10 +145,9 @@ if __name__ == '__main__':
                    'frac_train': 0.6,
                    'frac_test': 0.2,
                    'frac_valid': 0.2,
-                   'batch_size': 128,
+                   'batch_size': 256,
                    'steps': 100,
                    'seed': 42}
-
+    # experiment.add_tag("hyper-search")
     experiment.log_parameters(hyperparams)
-
-    train(hyperparams)
+    train(experiment, hyperparams)
