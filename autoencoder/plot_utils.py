8f486ee0509f9e10a21e3320f2f197d6730fc41b
Phil Travis <physicistphil@gmail.com>
2019-07-02 11:07:41 -0700

    Fixed file indices when plotting worst examples

--- autoencoder/plot_utils.py
+++ autoencoder/plot_utils.py
@@ -3,14 +3,15 @@ import matplotlib.pyplot as plt
 import matplotlib
 matplotlib.use('Agg')
 
+
 def plot_comparison(sess, data_test, X, output, hyperparams):
     output_test = output.eval(session=sess, feed_dict={X: data_test})
 
-    fig, axes = plt.subplots(nrows=2, ncols=3, figsize=(12, 8), sharex=True)
+    fig, axes = plt.subplots(nrows=3, ncols=4, figsize=(12, 8), sharex=True)
     fig.suptitle('Comparison of test set and reconstruction')
 
     np.random.seed(hyperparams['seed'])
-    randidx = np.random.randint(data_test.shape[0], size=(2, 3))
+    randidx = np.random.randint(data_test.shape[0], size=(3, 4))
 
     for x, y in np.ndindex((3, 4)):
         axes[x, y].plot(data_test[randidx[x, y]], label="Input")
@@ -36,9 +37,9 @@ def plot_worst(sess, data_train, X, output, hyperparams):
     fig.suptitle('Worst performing training examples')
 
     for x, y in np.ndindex((2, 3)):
-        axes[x, y].plot(data_train[idx_sq[- (x + y + 1)]], label="Input")
-        axes[x, y].plot(output_train[idx_sq[- (x + y + 1)]], label="Reconstruction")
-        axes[x, y].set_title("Index {}".format(idx_sq[- (x + y + 1)]))
+        axes[x, y].plot(data_train[idx_sq[- ((x + 3) * y + 1)]], label="Input")
+        axes[x, y].plot(output_train[idx_sq[- ((x + 3) * y + 1)]], label="Reconstruction")
+        axes[x, y].set_title("Index {}".format(idx_sq[- ((x + 3) * y + 1)]))
 
     axes[0, 0].legend()
 
