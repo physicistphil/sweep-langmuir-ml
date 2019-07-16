c98c45f8e2913a1e1707d2189ab89c70d12e5727
Phil Travis <physicistphil@gmail.com>
2019-07-16 13:42:59 -0700

    Implemented isat in trace generation (but this may not be too useful unless we get better sweep data).

--- simulator/generate.py
+++ simulator/generate.py
@@ -24,9 +24,39 @@ def generate_basic_trace(n, Vp, Te, vsweep, S=2e-6):
     I_esat = S * n_grid * e / np.sqrt(2 * np.pi * me)
     # calculate the current
     current = I_esat * np.sqrt(Te_grid) * np.exp(-e * (Vp_grid - vsweep_grid) / Te_grid)
-    # find where the bias voltage exceeds plasma potential and cap to I_esat
+    # find where the bias voltage exceeds plasma potential
     condition = Vp_grid < vsweep_grid
-    # cap the current when the bias voltage exceeds the plasma potential
+    # cap the current to I_esat
     current[condition] = I_esat[condition] * np.sqrt(Te_grid)[condition]
 
     return current
+
+
+# TODO: this is broken. Need to account for Ti in the meshgrid.
+# Ti grid is assumed to be the same size as the Te grid.
+# It may just be better to roll this into the function above.
+def generate_trace(ne, Vp, Te, Ti, vsweep, gas='He', S=2e-6):
+    if gas != 'he':
+        print("Different gasses are not implemented yet. Using Helium.")
+    # assuming helium fill gas
+    ni = ne / 4
+
+    current_e = generate_basic_trace(ne, Vp, Te, vsweep, S=2e-6)
+
+    mi = 6.646e-27
+    e = 1.602e-19
+
+    Ti = Ti * e  # convert to Joules
+
+    # make grid from inputs
+    ni_grid, Vp_grid, Ti_grid, vsweep_grid = np.meshgrid(ni, Vp, Ti, vsweep, indexing='ij')
+    # ion saturation current as Bohm current (assuming Ti << Te) (Amps / sqrt(eV))
+    I_isat = -0.6 * S * ni_grid * e / np.sqrt(mi)
+    # calculate the current
+    current_i = I_isat * np.sqrt(Ti_grid) * np.exp(e * (Vp_grid - vsweep_grid) / Ti_grid)
+    # find where the bias voltage is more negative than the plasma potential
+    isat_condition = Vp_grid > vsweep_grid
+    # cap current to I_isat
+    current_i[isat_condition] = I_isat[isat_condition] * np.sqrt(Ti_grid)[isat_condition]
+
+    return current_e + current_i
