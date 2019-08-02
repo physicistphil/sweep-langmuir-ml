ed54af69285610b9dbb12526384b77f22dd4ed45
Phil Travis <physicistphil@gmail.com>
2019-08-02 10:26:54 -0700

    Added function to grab voltage sweeps and current traces from the mirror data.

--- data_processor/preprocess.py
+++ data_processor/preprocess.py
@@ -29,12 +29,51 @@ def get_raw_mirror_data(input_size):
 
 
 def get_mirror_data(input_size):
-    # Divide by -11 because we measured the current backwards across 11 ohms. 
+    # Divide by -11 because we measured the current backwards across 11 ohms.
     # Current is mA.
     # Multiplier information can be found in the HDF5 file run descriptions.
     # This multiplier is only guaranteed to be correct for the mirror Langmuir sweeps that
     # were taken during the week of June 17th, 2018.
     return get_raw_mirror_data(input_size) / -11
+
+
+def get_mirror_data_with_sweeps(input_size):
+    # Multiply by 100 because that's what high-voltage probe was that was set for.
+    # This only applies to mirror data taken during the week of June 17th, 2018.
+    data_dir = "/home/phil/Desktop/sweeps/data/"
+
+    try:
+        dataset = np.load(data_dir + 'data_with_sweeps')['dataset']
+    except FileNotFoundError:
+        f = [lapd.File(data_dir + "09_500G_flat_sweep_vf_correct2.hdf5"),
+             lapd.File(data_dir + "10_500G_7to8_750Gother_sweep_vf.hdf5"),
+             lapd.File(data_dir + "11_500G_7to8_1000Gother_sweep_vf.hdf5"),
+             lapd.File(data_dir + "12_500G_7to8_1250Gother_sweep_vf.hdf5"),
+             lapd.File(data_dir + "13_500G_7to8_1500Gother_sweep_vf.hdf5")]
+
+        sweep_len = f[0].read_data(2, 1, silent=True, shotnum=[1])['signal'].shape[1]
+        trace_len = f[0].read_data(2, 2, silent=True, shotnum=[1])['signal'].shape[1]
+        assert sweep_len == trace_len, "Voltage and current traces not the same length"
+
+        # Get the sweeps and traces from the data files.
+        # Reduce the length of the data from 65536 to input_size (usually 500).
+        stride = int(trace_len / input_size)
+        data_sweep = np.concatenate([_.read_data(2, 1, silent=True)['signal']
+                                     [:, 0:input_size * stride:stride] for _ in f], axis=0)
+        data_trace = np.concatenate([_.read_data(2, 2, silent=True)['signal']
+                                     [:, 0:input_size * stride:stride] for _ in f], axis=0)
+        # Multiply the sweeps by 100 because it was measured through a x100 HV probe.
+        data_sweep = 100 * data_sweep
+        # Flip the traces (so it's consistent with the voltage sweep).
+        data_trace = -data_trace
+        # Remove the offset of the traces by averaging the first 10 points and subtracting it.
+        data_trace -= np.mean(data_trace[:, 0:10], axis=1)[:, np.newaxis]
+        # Merge the sweep and trace together like we do for the synthetic sweeps.
+        dataset = np.concatenate((data_sweep, data_trace), axis=1)
+        # Cache the result on disk (NVME SSDs FTW) to save time.
+        np.savez(data_dir + 'data_with_sweeps', dataset=dataset)
+
+    return dataset
 
 
 # Shuffle the data and split it into training, testing, and validation sets based on hyperparams
@@ -52,7 +91,7 @@ def shuffle_split_data(data, hyperparams):
 
 # Add a random offset to the data.
 def add_offset_to_half(X, hyperparams, epoch=0):
-    offset_scale = 0.2 * np.ptp(X[:, hyperparams['n_inputs']:], axis=1)
+    offset_scale = hyperparams['offset_scale'] * np.ptp(X[:, hyperparams['n_inputs']:], axis=1)
     # Add the current epoch so we get some variety over the entire training run.
     np.random.seed(hyperparams['seed'] + epoch)
     offsets = np.random.uniform(-offset_scale, offset_scale)
@@ -63,7 +102,7 @@ def add_offset_to_half(X, hyperparams, epoch=0):
 
 # Add noise to the data.
 def add_noise_to_half(X, hyperparams, epoch=0):
-    noise_scale = 0.2 * np.ptp(X[:, hyperparams['n_inputs']:], axis=1)
+    noise_scale = hyperparams['noise_scale'] * np.ptp(X[:, hyperparams['n_inputs']:], axis=1)
     # Add the current epoch so we get some variety over the entire training run.
     np.random.seed(hyperparams['seed'] + epoch)
     noise = np.random.normal(np.zeros((X.shape[0], hyperparams['n_inputs'])),
