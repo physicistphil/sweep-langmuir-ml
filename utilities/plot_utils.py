Fixed incorrect data scaling and incorrect data plotting cutoff for physics-based model comparison.

utilities/plot_utils.py
@@ -74,7 +74,8 @@ def phys_plot_comparison(sess, data_test, data_mean, data_ptp,
                                                             X_ptp: data_ptp})
 
     # Reshape in case our data is 2d (which would be the case for a convolutional autoencoder)
-    data_test = np.reshape(data_test, (size, -1))
+    # Rescale to original values
+    data_test = np.reshape(data_test, (size, -1)) * data_ptp + data_mean
     output_test = np.reshape(output_test, (size, -1))
 
     fig, axes = plt.subplots(nrows=3, ncols=4, figsize=(12, 8), sharex=True)
@@ -84,7 +85,7 @@ def phys_plot_comparison(sess, data_test, data_mean, data_ptp,
     randidx = np.random.randint(size, size=(3, 4))
 
     for x, y in np.ndindex((3, 4)):
-        axes[x, y].plot(data_test[randidx[x, y], (size // 2):], label="Input")
+        axes[x, y].plot(data_test[randidx[x, y], hyperparams['n_inputs']:], label="Input")
         axes[x, y].plot(output_test[randidx[x, y]], label="Reconstruction")
         axes[x, y].set_title("Index {}".format(randidx[x, y]))
 